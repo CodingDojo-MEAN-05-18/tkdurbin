c067a00c5e2e86ee078c0b6b4b1eb7381e6ff69f
Todd <tkdurbin@aol.com>
2018-07-16 16:23:27 -0500

    updated assignments

--- angular/switchboard/src/app/app.component.ts
+++ angular/switchboard/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  switches = [true, true, true, true, true, true, true, true, true, true];
+  flipSwitch(idx) {
+    this.switches[idx] = !this.switches[idx];
+  }
 }
