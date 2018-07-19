3cfd3cb154c208869c2dff3cd5df9ebd6edce4f9
Todd <tkdurbin@aol.com>
2018-07-19 10:47:20 -0500

    updates

--- angular/layout/src/app/app.module.ts
+++ angular/layout/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AlphaComponent } from './alpha/alpha.component';
@@ -14,7 +16,9 @@ import { GammaComponent } from './beta/gamma/gamma.component';
     GammaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
